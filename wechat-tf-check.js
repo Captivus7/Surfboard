dc38e34fd45abd255031d6e99a9c4c98c83744c3
自律的土狗 <99650564+Captivus7@users.noreply.github.com>
2025-05-11 22:24:30 +0800

    更新 wechat-tf-check.js

--- wechat-tf-check.js
+++ wechat-tf-check.js
@@ -1,23 +1,20 @@
 // 微信 TestFlight 状态监控脚本（用于 Surge）
+const tfURL = "https://testflight.apple.com/join/Gy4BzQrs"; // 微信 TF 链接
+const tfName = "微信 TF";
+const noticeTitle = "TestFlight 状态监控";
 
-const url = "https://testflight.apple.com/join/Gy4BzQrs";  // 微信 TF 链接
+$httpClient.get(tfURL, function (error, response, data) {
+    if (error) {
+        console.log(`${tfName} 请求失败: ${error}`);
+        $notification.post(noticeTitle, tfName, "请求失败");
+        return $done();
+    }
 
-$httpClient.get(url, function (error, response, data) {
-  if (error) {
-    $notification.post("微信 TF 状态检查", "请求失败", error);
+    if (data.includes("This beta is full")) {
+        console.log(`${tfName} 当前满员`);
+    } else {
+        console.log(`${tfName} 已开放`);
+        $notification.post(noticeTitle, tfName, "已开放，可前往加入测试");
+    }
     $done();
-    return;
-  }
-
-  if (data.includes("This beta is full")) {
-    console.log("微信 TF 当前未开放");
-    $done();
-  } else {
-    $notification.post("微信 TF 开放啦！", "立即前往 TestFlight 安装", "点击查看", {
-      url: url
-    });
-    console.log("微信 TF 已开放");
-  }
-
-  $done();
 });
